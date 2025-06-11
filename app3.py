811479023290501784380dd46a55f2e81ea8f15e
MrERSOY <cagatayersoy99@gmail.com>
2025-06-11 03:38:05 +0300

    Devam ke

---
+++ app3.py
@@ -0,0 +1,6 @@
+# yaş 18 ile 65 arasında olacak
+
+age = 22
+# if age >= 18 and age <= 65:
+if 18 <= age <= 65:
+    print("Uygun")
---
+++ app5.py
@@ -0,0 +1,3 @@
+
+for number in range(11):
+    print("Bir mesaj gönderiliyor...", number + 1, (number + 1) * ".")
